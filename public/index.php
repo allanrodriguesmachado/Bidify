1e214a9ec18c450673485b630b052566243b3cc6
Allan Rodrigues - Developer <consultoria01@shibata.corp.br>
2025-06-13 15:05:34 -0300

    Creat new Test

--- public/index.php
+++ public/index.php
@@ -5,13 +5,21 @@ use App\Model\{Avaliador, Lance, Leilao, Usuario};
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $leilao = new Leilao('Fiat 147 0KM');
+
 $userOne = new Usuario('Allan');
-$userTwo = new Usuario('Allan');
+$userTwo = new Usuario('Rodrigues');
+$userThree = new Usuario('Machado');
+$userFour = new Usuario('Fernando');
+
 $lanceOne = new Lance($userOne,'3000');
 $lanceTwo = new Lance($userTwo,'2000');
+$lanceTree = new Lance($userThree,'5000');
+$lanceFour = new Lance($userFour,'1000');
 
 $leilao->recebeLance($lanceOne);
 $leilao->recebeLance($lanceTwo);
+$leilao->recebeLance($lanceTree);
+$leilao->recebeLance($lanceFour);
 
 $avaliador = new Avaliador();
 $avaliador->avalia($leilao);
