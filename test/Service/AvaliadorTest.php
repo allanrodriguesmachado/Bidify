a1a8d5dec5d5d110e7260acd2eca422539787c44
Allan Rodrigues <allan.rodrigues14@hotmail.com>
2025-06-17 21:30:51 -0300

    Refatorando Test

--- test/Service/AvaliadorTest.php
+++ test/Service/AvaliadorTest.php
@@ -2,56 +2,35 @@
 
 namespace App\Test\Service;
 
+use JetBrains\PhpStorm\NoReturn;
 use App\Model\{Lance, Leilao, Usuario, Avaliador};
+use PHPUnit\Framework\Attributes\{Before, DataProvider};
 use PHPUnit\Framework\TestCase;
 
 class AvaliadorTest extends TestCase
 {
-    public function test()
+    private readonly Avaliador $avaliador;
+    #[NoReturn] #[Before]
+    protected function initialize(): void
     {
-        $avaliador = $this->getAvaliador();
-
-        $this->assertEquals("3000", $avaliador->getMaiorValor());
+        $this->avaliador = new Avaliador();
     }
 
-    public function testMenorValor()
+    #[DataProvider('getAvaliador')]
+    public function testMenorValor(Leilao $leilao)
     {
-        $avaliador = $this->getAvaliador();
-
-        $this->assertEquals("2000", $avaliador->getMenorValor());
+        $this->avaliador->avalia($leilao);
+        $this->assertEquals("2000", $this->avaliador->getMenorValor());
     }
 
-    public function testPegarOsTresMaioresValores(): void
+    #[DataProvider('getAvaliador')]
+    public function testMaiorValor(Leilao $leilao)
     {
-        $leilao = new Leilao('Fiat 147 0KM');
-
-        $qtd = mb_strlen($leilao->getDesciptions());
-
-        $userOne = new Usuario('Allan');
-        $userTwo = new Usuario('Rodrigues');
-        $userThree = new Usuario('Machado');
-        $userFour = new Usuario('Fernando');
-
-        $leilao->recebeLance(new Lance($userOne, '3000'));
-        $leilao->recebeLance(new Lance($userTwo, '2000'));
-        $leilao->recebeLance(new Lance($userThree, '5000'));
-        $leilao->recebeLance(new Lance($userFour, '1000'));
-
-        $avaliador = new Avaliador();
-        $avaliador->avalia($leilao);
-
-        $maiores = $avaliador->getMaioresLances();
-
-        $this->assertCount(3, $maiores);
-
-        $this->assertLessThan(14, $qtd, "Atencao! Atingiu o limite");
-
-        $this->assertEquals("5000", $maiores[0]->getValor(), "Maior Valor");
-        $this->assertEquals("3000", $maiores[1]->getValor(), "Medio Valor");
-        $this->assertEquals("2000", $maiores[2]->getValor(), "Baixo Valor");
+        $this->avaliador->avalia($leilao);
+        $this->assertEquals("3000", $this->avaliador->getMaiorValor());
     }
 
-    public function getAvaliador(): Avaliador
+    public static function getAvaliador(): array
     {
         $leilao = new Leilao('Fiat 147 0KM');
         $userOne = new Usuario('Allan');
@@ -62,8 +41,8 @@ class AvaliadorTest extends TestCase
         $leilao->recebeLance($lanceOne);
         $leilao->recebeLance($lanceTwo);
 
-        $avaliador = new Avaliador();
-        $avaliador->avalia($leilao);
-        return $avaliador;
+        return [
+            [$leilao]
+        ];
     }
 }
